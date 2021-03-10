5673aae4d33c39a67780be1ad2d122a378d8c4be
LuanAccioly <acciolyluan@gmail.com>
2021-03-10 19:37:46 -0300

    feature: add challenge box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { ExperienceBar } from "../components/Experiencebar";
 import { Profile } from '../components/profiles';
 import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css'
+import { ChallengeBox } from '../components/ChallengeBox';
  
 export default function Home() {
   return (
@@ -21,7 +22,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
