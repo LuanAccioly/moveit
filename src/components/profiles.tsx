353b2b7b341a07ff2a5e557ba467e50cd1f3d4f5
LuanAccioly <acciolyluan@gmail.com>
2021-03-16 20:30:09 -0300

    feature: adding functionality to the challenge buttons

--- src/components/profiles.tsx
+++ src/components/profiles.tsx
@@ -1,5 +1,8 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 export function Profile(){
+    const {level } = useContext(ChallengesContext);
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/LuanAccioly.png" alt="Luan Accioly"/>
@@ -8,9 +11,9 @@ export function Profile(){
                 <strong>Luan Accioly</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
     )
-}
+} 
